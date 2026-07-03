4dd2523aa38269f68536a007a119de9d392313e2
SHAHZAIB <shahzaibkhanmsk1@gmail.com>
2026-07-03 21:05:25 +0500

    commen

--- assets/js/script.js
+++ assets/js/script.js
@@ -27,62 +27,65 @@
 // window.onscroll = function () {
 //   let sections = document.querySelectorAll(".section");
 //   let scroll = window.scrollY;
-  
+
 //   sections.forEach(sec => {
 //     if (scroll >= sec.offsetTop - 200 && scroll < sec.offsetTop + sec.offsetHeight) {
-      
+
 //       document.querySelectorAll(".nav-links ul li a").forEach(a => a.classList.remove("active"));
-      
+
 //       document.querySelector('.nav-links ul li a[href="#' + sec.id + '"]').classList.add("active");
 //     }
 //   });
 // };
 
 window.addEventListener("scroll", () => {
-  const sections = document.querySelectorAll(".section");
-  const links = document.querySelectorAll(".nav-links a");
+    const sections = document.querySelectorAll(".section");
+    const links = document.querySelectorAll(".nav-links a");
 
-  sections.forEach(sec => {
-    if (
-      window.scrollY >= sec.offsetTop - 200 &&
-      window.scrollY < sec.offsetTop + sec.offsetHeight
-    ) {
-      links.forEach(link => link.classList.remove("active"));
+    sections.forEach(sec => {
+        if (
+            window.scrollY >= sec.offsetTop - 200 &&
+            window.scrollY < sec.offsetTop + sec.offsetHeight
+        ) {
+            links.forEach(link => link.classList.remove("active"));
 
-      const activeLink = document.querySelector(
-        `.nav-links a[href="#${sec.id}"]`
-      );
+            const activeLink = document.querySelector(
+                `.nav-links a[href="#${sec.id}"]`
+            );
 
-      if (activeLink) {
-        activeLink.classList.add("active");
-      }
-    }
-  });
-});
-const reveals = document.querySelectorAll(".reveal");
-
-const observer = new IntersectionObserver((entries) => {
-    entries.forEach(entry => {
-        if(entry.isIntersecting){
-            entry.target.classList.add("active");
+            if (activeLink) {
+                activeLink.classList.add("active");
+            }
         }
     });
-},{
-    threshold:0.2
 });
+const reveals = document.querySelectorAll(".reveal");
+if (reveals) {
 
-reveals.forEach(section => {
-    observer.observe(section);
-});
+    const observer = new IntersectionObserver((entries) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add("active");
+            }
+        });
+    }, {
+        threshold: 0.2
+    });
 
-    let typed = new Typed("#typing" ,{
-        strings :["Web Developer","Web Designer","Fronten Developer"],
-        typeSpeed : 100,
-        backSpeed : 50,
-        loop:true
+    reveals.forEach(section => {
+        observer.observe(section);
+    });
+}
+if (document.querySelector(".typing")) {
+    let typed = new Typed("#typing", {
+        strings: ["Web Developer", "Web Designer", "Frontend Developer"],
+        typeSpeed: 100,
+        backSpeed: 50,
+        loop: true
     })
+}
 
-    // Dark Mode
+// Dark Mode
 const darkModeBtn = document.getElementById("darkMode");
 const body = document.body;
 const icon = darkModeBtn.querySelector("i");
@@ -93,9 +96,9 @@ if (localStorage.getItem("theme") === "dark") {
     icon.classList.remove("bi-moon-fill");
     icon.classList.add("bi-sun-fill");
 }
-
 darkModeBtn.addEventListener("click", () => {
     body.classList.toggle("dark");
+
 
     if (body.classList.contains("dark")) {
         icon.classList.remove("bi-moon-fill");
@@ -106,74 +109,15 @@ darkModeBtn.addEventListener("click", () => {
         icon.classList.add("bi-moon-fill");
         localStorage.setItem("theme", "light");
     }
+
 });
-//project value counter
-// const projectValue = document.getElementById("projects-value");
-// let projectCount = 0;
-// const targetCount = 16;
-// if(projectValue){
-//     const interval = setInterval(() => {
-//         projectCount++;
-//         projectValue.textContent = projectCount;
-//         if (projectCount >= targetCount) {
-//             clearInterval(interval);
-//         }
-//     }, 100);
-// }
-
-// const clientsValue = document.getElementById("clients-value");
-// let clientsCount = 0;
-// const targetClientsCount = 40;
-// if(clientsValue){
-//     const intervalClients = setInterval(() => {
-//         clientsCount++;
-//         clientsValue.textContent = clientsCount;
-//         if (clientsCount >= targetClientsCount) {
-//             clearInterval(intervalClients);
-//         }
-//     }, 50);
-// }
-// const experienceValue = document.getElementById("experience-value");
-// let experienceCount = 0;
-// const targetExperienceCount = 2;
-// if(experienceValue){
-//     const intervalExperience = setInterval(() => {
-//         experienceCount++;
-//         experienceValue.textContent = experienceCount;
-//         if (experienceCount >= targetExperienceCount) {
-//             clearInterval(intervalExperience);
-//         }
-//     }, 100);
-// }
-//     const dedicationValue = document.getElementById("dedication-value");
-//     let dedicationCount = 0;
-//     const targetDedicationCount = 100;
-//     if(dedicationValue){
-//         const intervalDedication = setInterval(() => {
-//             dedicationCount++;
-
-//             dedicationValue.textContent = dedicationCount;
-//             if (dedicationCount >= targetDedicationCount) {
-//                 clearInterval(intervalDedication);
-//             }
-//         }, 20);
-//     }
-//     const satificationValue = document.getElementById("satification-value");
-//     let satificationCount = 0;
-//     const targetSatificationCount = 100;
-//     if(satificationValue){
-//         const intervalSatification = setInterval(() => {
-//             satificationCount++;
-
-//             sastificationValue.textContent = sastificationCount;
-//             if (sastificationCount >= targetSastificationCount) {
-//                 clearInterval(intervalSastification);
-//             }
-//         }, 20);
-//     }
 function startCounter(id, target, speed) {
     const element = document.getElementById(id);
     let started = false;
+    if (!element) {
+        return;
+    }
+
 
     const observer = new IntersectionObserver((entries) => {
         entries.forEach(entry => {
@@ -202,3 +146,93 @@ startCounter("clients-value", 40, 50);
 startCounter("experience-value", 2, 300);
 startCounter("dedication-value", 100, 20);
 startCounter("satification-value", 100, 20);
+startCounter("html", 90, 20);
+startCounter("css", 85, 20);
+startCounter("javascript", 60, 20);
+startCounter("bootstrap", 90, 20);
+
+// accordion button
+const accordionButtons = document.querySelectorAll(".accordion-btn button");
+accordionButtons.forEach(button => {
+    const downArrow = button.querySelector(".down-arrow i");
+    button.addEventListener("click", () => {
+        const accordionContent = button.parentElement.nextElementSibling;
+        const isOpen = accordionContent.style.display === "block";
+
+        accordionButtons.forEach(btn => {
+            const content = btn.parentElement.nextElementSibling;
+            const arrow = btn.querySelector(".down-arrow i");
+            content.style.display = "none";
+            arrow.classList.remove("bi-caret-up-fill");
+            arrow.classList.add("bi-caret-down-fill");
+        });
+        if (!isOpen) {
+            accordionContent.style.display = "block";
+            downArrow.classList.remove("bi-caret-down-fill");
+            downArrow.classList.add("bi-caret-up-fill");
+        }
+
+    })
+});
+
+// 
+const toast = document.getElementById("toast");
+
+if (typeof emailjs !== "undefined") {
+    emailjs.init("UJb-VdvnbrbEsOb59");
+}
+
+const form = document.getElementById("contact-form");
+
+if (form) {
+
+    form.addEventListener("submit", function (event) {
+
+        event.preventDefault();
+
+        const name =
+            document.getElementById("name").value;
+
+        const email =
+            document.getElementById("email").value;
+
+        const message =
+            document.getElementById("message").value;
+
+        emailjs.send(
+            "service_70n0uxe",
+            "template_j8ajxuf",
+            {
+                name: name,
+                email: email,
+                message: message
+            }
+        )
+            .then(function () {
+                toast.hidden = false;
+                toast.innerHTML = "Form submitted successfully ✅";
+                toast.classList.add("show");
+                setTimeout(() => {
+                    toast.classList.remove("show");
+                    toast.hidden = true;
+                }, 5000);
+
+                form.reset();
+
+            })
+            .catch(function (error) {
+
+                console.log(error);
+                toast.hidden = false;
+                toast.innerHTML = "Failed To Send Email ❌";
+                toast.classList.add("show");
+                setTimeout(() => {
+                    toast.classList.remove("show");
+                    toast.hidden = true;
+                }, 5000);
+
+            });
+
+    });
+
+}
